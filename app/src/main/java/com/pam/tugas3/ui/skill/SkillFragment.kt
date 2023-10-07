3873a753591c617e2f5dcccb85c7ea57f1f082d9
rhmnx <116880430+rhmnx@users.noreply.github.com>
2023-10-07 10:52:08 +0700

    Tugas 4

--- app/src/main/java/com/pam/tugas3/ui/skill/SkillFragment.kt
+++ app/src/main/java/com/pam/tugas3/ui/skill/SkillFragment.kt
@@ -11,29 +11,14 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.pam.tugas3.R
-import kotlin.collections.ArrayList
 import java.util.*
 
-private const val t10 = "t1"
-private const val t20 = "t2"
-
 class SkillFragment : Fragment() {
-    private var t1: String? = null
 
-    private lateinit var adapter : Adaptor
+    private lateinit var adapter: Adaptor
     private lateinit var recyclerView: RecyclerView
     private lateinit var searchView: SearchView
-    private lateinit var skillArrayList : ArrayList<Skill>
-    private lateinit var imageId : Array<Int>
-    private lateinit var heading : Array<String>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            t1 = it.getString(t10)
-            t1 = it.getString(t20)
-        }
-    }
+    private var skillArrayList = ArrayList<Skill>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,7 +34,6 @@ class SkillFragment : Fragment() {
         recyclerView = view.findViewById(R.id.recycler_view_skill)
         recyclerView.layoutManager = layoutManager
         recyclerView.setHasFixedSize(true)
-        dataInitialize()
         adapter = Adaptor(skillArrayList)
         recyclerView.adapter = adapter
         searchView = view.findViewById(R.id.search_action)
@@ -67,87 +51,41 @@ class SkillFragment : Fragment() {
                 filterList(newText)
                 return true
             }
-
         })
 
+        getUserData()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        t1 = null
-    }
-
-    private fun navigateToDetail(extraName: String){
+    private fun navigateToDetail(extraName: String) {
         findNavController().navigate(SkillFragmentDirections.actionSkillToDetail(extraName))
     }
 
     private fun filterList(query: String?) {
-
         if (query != null) {
             val filteredList = ArrayList<Skill>()
             for (i in skillArrayList) {
-                if (i.heading.lowercase(Locale.ROOT).contains(query)) {
+                if (i.heading.lowercase(Locale.ROOT).contains(query.lowercase(Locale.ROOT))) {
                     filteredList.add(i)
                 }
             }
 
             if (filteredList.isEmpty()) {
                 Toast.makeText(context, "No Data Found", Toast.LENGTH_SHORT).show()
-            }
-            else {
+            } else {
                 adapter.setFilteredList(filteredList)
             }
         }
     }
 
-    private fun dataInitialize(){
-        skillArrayList = arrayListOf<Skill>()
-
-        imageId = arrayOf(
-            R.drawable.cpp,
-            R.drawable.csharp,
-            R.drawable.flutter,
-            R.drawable.js,
-            R.drawable.kotlin,
-            R.drawable.php,
-            R.drawable.python,
-            R.drawable.html,
-            R.drawable.css,
-            R.drawable.mysql,
-            R.drawable.cpp,
-            R.drawable.csharp,
-            R.drawable.flutter,
-            R.drawable.js,
-        )
-
-        heading = arrayOf(
-            getString(R.string.text_cpp),
-            getString(R.string.text_csharp),
-            getString(R.string.text_flutter),
-            getString(R.string.text_js),
-            getString(R.string.text_kotlin),
-            getString(R.string.text_php),
-            getString(R.string.text_python),
-            getString(R.string.text_html),
-            getString(R.string.text_css),
-            getString(R.string.text_mysql),
-            getString(R.string.text_cpp),
-            getString(R.string.text_csharp),
-            getString(R.string.text_flutter),
-            getString(R.string.text_js),
-        )
-        getUserData()
-
-    }
-
     private fun getUserData() {
+        val skillArray = resources.getStringArray(R.array.array_skill)
 
-        for (i in imageId.indices){
-            val skill = Skill(imageId[i],heading[i])
+        val imageArray = resources.obtainTypedArray(R.array.array_image)
+
+        for (i in skillArray.indices) {
+            val skill = Skill(imageArray.getResourceId(i, 0), skillArray[i])
             skillArrayList.add(skill)
-
         }
-
+        imageArray.recycle()
     }
-
 }
